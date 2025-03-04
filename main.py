b7ebba771bb29af1d253cd9a522c084b41fbfa10
lorenzobruni-dev <lorenzobruni40@gmail.com>
2025-03-05 00:54:48 +0100

    Cleaning repo

--- main.py
+++ main.py
@@ -56,7 +56,6 @@ async def on_member_join(member):
               font=font)
     badgepath = f"badge_{member.id}.png"
     badge.paste(avatar, (10, 25), avatar)
-    badge.save(f"imgBadge/{badgepath}")
 
     matricola_counter += 1
 
